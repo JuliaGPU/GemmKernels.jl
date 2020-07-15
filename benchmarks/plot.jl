12836956ea9aaa8fc14c2d29a54a9aa6ecf35faa
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-07-15 15:28:05 +0200

    Add benchmarks for WMMA (#9)
    * Add benchmarks for WMMA
    
    * Handle 'n/a' in profile results

---
+++ benchmarks/plot.jl
@@ -0,0 +1,48 @@
+using CSV
+using DataFrames
+using Plots
+using Statistics
+
+pyplot()
+
+function convert_to_array(str)
+    els = split(str, ',')
+
+    if "n/a" in els
+        @warn "Benchmark contains 'n/a' values. These will be ignored."
+        filter!(el -> el != "n/a", els)
+    end
+
+    return parse.(Float64, els)
+end
+
+titles = Dict(
+              "wmma" => "Performance of mixed-precision GEMM",
+             )
+
+labels = Dict(
+              "cublas" => "cuBLAS",
+              "gemmkernels" => "Our implementation"
+             )
+
+dir = ARGS[1]
+cd(dir)
+
+for file in readdir()
+    if isfile(file) && splitext(basename(file))[2] == ".csv"
+        label = labels[splitext(basename(file))[1]]
+
+        df = DataFrame(CSV.File(file))
+
+        N = df[!, :N]
+        runtime_arr = convert_to_array.(df[!, :runtime]) .* 1e3 # in ps
+        tflops = [2 * N[i] ^ 3 ./ runtime_arr[i] for i = 1 : length(N)]
+
+        plot!(N, mean.(tflops), label=label, xscale=:log2, markershape=:circle, ribbon=std.(tflops), fillalpha=.5)
+    end
+end
+
+title!(titles[dir])
+xlabel!("N")
+ylabel!("TFLOPS")
+savefig("plot.pdf")
---
+++ benchmarks/profile.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# Usage: ./profile.sh <script>
+if [[ $# < 1 ]]; then
+    echo "Usage: $0 <script>" 1>&2
+    exit 1
+fi
+
+# Julia path must be set
+if [[ -z ${JULIA_PATH+x} ]]; then
+    echo "Please set JULIA_PATH to the root path of the Julia repository." 1>&2
+    echo "Example: export JULIA_PATH=~/src/julia/" 1>&2
+    exit 1
+fi
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+SCRIPT="$1"
+OUTPUT_FILE="${SCRIPT%.*}.csv"
+
+printf "N,runtime\n" >$OUTPUT_FILE
+
+for i in {7..14}; do
+    N=$((2**i))
+
+    # runtime is in nanoseconds
+    runtime=$(LD_LIBRARY_PATH=${JULIA_PATH}/usr/lib nv-nsight-cu-cli --profile-from-start off -f --csv --units base --metrics 'gpu__time_duration.avg' ${JULIA_PATH}/julia $SCRIPT $N $N $N 2>/dev/null | grep 'gpu__time_duration' | awk -F',' '{print $NF}' | sed 's/"//g' | paste -sd ',')
+
+    printf "$N,\"$runtime\"\n" >>$OUTPUT_FILE
+done
