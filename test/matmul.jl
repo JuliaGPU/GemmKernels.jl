ae5f8a8a1eecd5487517ddf61a83e12d12176ac6
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-07-16 05:29:51 -0400

    Add scaling test

--- test/matmul.jl
+++ test/matmul.jl
@@ -12,6 +12,9 @@ using GemmKernels
             N in [128, 256],
             K in [128, 256]
 
+            alpha = 2
+            beta  = 3
+
             a_h = rand(Float16, (M, K)) / sqrt(Float16(K))
             b_h = rand(Float16, (K, N)) / sqrt(Float16(K))
             c_h = rand(Float32, (M, N))
@@ -35,42 +38,15 @@ using GemmKernels
                 global_d_layout = Layout.AlignedColMajor{Float32},
                                     )
 
-            GemmKernels.matmul(a, b, c, d, conf)
+            GemmKernels.matmul(a, b, c, d, conf;
+                               transform_shared_to_regs_a = Transform.Elementwise(x -> x * alpha),
+                               transform_shared_to_regs_c = Transform.Elementwise(x -> x * beta))
 
             # Transpose outputs, if necessary
             new_a_h = transpose_a ? transpose(a_h) : a_h
             new_b_h = transpose_b ? transpose(b_h) : b_h
 
-            @test all(isapprox.(Float32.(new_a_h) * Float32.(new_b_h) + c_h, Array(d); rtol = sqrt(eps(Float16))))
-        end
-    end
-
-    @testset "WMMA GEMM + scaling" begin
-        @testset "(M = $M, N = $N, K = $K, alpha = $alpha)" for M in [128, 256],
-            N in [128, 256],
-            K in [128, 256],
-            alpha in [2, 5]
-
-            a_h = rand(Float16, (M, K)) / sqrt(Float16(K))
-            b_h = rand(Float16, (K, N)) / sqrt(Float16(K))
-            c_h = rand(Float32, (M, N))
-
-            a   = CuArray(a_h)
-            b   = CuArray(b_h)
-            c   = CuArray(c_h)
-            d   = similar(c)
-
-            conf = GemmKernels.get_config(
-                gemm_shape = (M = M, N = N, K = K),
-                operator = Operator.WMMAOp{16, 16, 16},
-                global_a_layout = Layout.AlignedColMajor{Float16},
-                global_c_layout = Layout.AlignedColMajor{Float32}
-                                    )
-
-            GemmKernels.matmul(a, b, c, d, conf;
-                          transform_shared_to_regs_c = Transform.Elementwise(x -> x * alpha))
-
-            @test all(isapprox.(Float32.(a_h) * Float32.(b_h) + alpha * c_h, Array(d); rtol = sqrt(eps(Float16))))
+            @test all(isapprox.(alpha * Float32.(new_a_h) * Float32.(new_b_h) + beta * c_h, Array(d); rtol = sqrt(eps(Float16))))
         end
     end
 
