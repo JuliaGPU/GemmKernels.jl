c7ab0d09f8d19bfd7f3e1b0550cc9e2e37c2bab9
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-07-27 16:10:20 +0200

    Clean up tests

--- test/matmul.jl
+++ test/matmul.jl
@@ -53,78 +53,89 @@ using GemmKernels
         end
     end
 
-    @testset "WMMA Complex GEMM ($( !conjugate_a ? 'N' : 'C' )$( !conjugate_b ? 'N' : 'C' ))" for conjugate_a = [false, true],
-        conjugate_b = [false, true]
-        @testset "Transpose A: $(transpose_a), Transpose B: $(transpose_b)" for transpose_a = [false, true],
-            transpose_b = [false, true]
+    function complex_transform_to_letter(conjugate::Bool, transpose::Bool)
+        # key = (conjugate, transpose)
+        d = Dict(
+                 (false, false) => 'N',
+                 (true, false) => 'C',
+                 (false, true) => 'T',
+                 (true, true) => 'H'
+                )
 
-            @testset "(M = $M, N = $N, K = $K)" for M in [128],
-                N in [128],
-                K in [128]
+        return d[(conjugate, transpose)]
+    end
 
-                a_h = rand(Complex{Float16}, (M, K)) / sqrt(Float16(K));
-                b_h = rand(Complex{Float16}, (K, N)) / sqrt(Float16(K));
-                c_h = rand(Complex{Float32}, (M, N));
+    @testset "WMMA Complex GEMM ($( complex_transform_to_letter(conjugate_a, transpose_a) )$( complex_transform_to_letter(conjugate_b, transpose_b) ))" for conjugate_a = [false, true],
+        conjugate_b = [false, true],
+        transpose_a = [false, true],
+        transpose_b = [false, true]
 
-                # Transpose input if necessary
-                a_h = transpose_a ? transpose(a_h) : a_h
-                b_h = transpose_b ? transpose(b_h) : b_h
+        @testset "(M = $M, N = $N, K = $K)" for M in [128, 256],
+            N in [128, 256],
+            K in [128, 256]
 
-                a = CuArray(a_h);
-                b = CuArray(b_h);
-                c = CuArray(c_h);
-                d = similar(c);
+            a_h = rand(Complex{Float16}, (M, K)) / sqrt(Float16(K));
+            b_h = rand(Complex{Float16}, (K, N)) / sqrt(Float16(K));
+            c_h = rand(Complex{Float32}, (M, N));
 
-                conf = GemmKernels.get_config(
-                        gemm_shape = (M = M, N = N, K = K),
-                        operator = Operator.WMMAComplexOp{16, 16, 16},
+            # Transpose input if necessary
+            a_h = transpose_a ? transpose(a_h) : a_h
+            b_h = transpose_b ? transpose(b_h) : b_h
 
-                        global_a_layout = transpose_a ? Layout.InterleavedRowMajor{Float16} : Layout.InterleavedColMajor{Float16},
-                        global_b_layout = transpose_b ? Layout.InterleavedRowMajor{Float16} : Layout.InterleavedColMajor{Float16},
-                        global_c_layout = Layout.InterleavedColMajor{Float32},
-                        global_d_layout = Layout.InterleavedColMajor{Float32},
+            a = CuArray(a_h);
+            b = CuArray(b_h);
+            c = CuArray(c_h);
+            d = similar(c);
 
-                        shared_a_layout = transpose_a ? Layout.Padded{Layout.SplitRowMajor{Float16}, 8} : Layout.Padded{Layout.SplitColMajor{Float16}, 8},
-                        shared_b_layout = transpose_b ? Layout.Padded{Layout.SplitRowMajor{Float16}, 8} : Layout.Padded{Layout.SplitColMajor{Float16}, 8},
-                        shared_c_layout = Layout.SplitColMajor{Float32},
-                        shared_d_layout = Layout.SplitColMajor{Float32},
+            conf = GemmKernels.get_config(
+                    gemm_shape = (M = M, N = N, K = K),
+                    operator = Operator.WMMAComplexOp{16, 16, 16},
 
-                        warps_per_block = 8,
+                    global_a_layout = transpose_a ? Layout.InterleavedRowMajor{Float16} : Layout.InterleavedColMajor{Float16},
+                    global_b_layout = transpose_b ? Layout.InterleavedRowMajor{Float16} : Layout.InterleavedColMajor{Float16},
+                    global_c_layout = Layout.InterleavedColMajor{Float32},
+                    global_d_layout = Layout.InterleavedColMajor{Float32},
 
-                        compute_warp = (M = 16, N = 32, K = 16),
+                    shared_a_layout = transpose_a ? Layout.Padded{Layout.SplitRowMajor{Float16}, 8} : Layout.Padded{Layout.SplitColMajor{Float16}, 8},
+                    shared_b_layout = transpose_b ? Layout.Padded{Layout.SplitRowMajor{Float16}, 8} : Layout.Padded{Layout.SplitColMajor{Float16}, 8},
+                    shared_c_layout = Layout.SplitColMajor{Float32},
+                    shared_d_layout = Layout.SplitColMajor{Float32},
 
-                        block_shape = (M = 64, N = 64, K = 32),
+                    warps_per_block = 8,
 
-                        mem_a_warp = transpose_a ? (M = 4, K = 32) : (M = 64, K = 2),
-                        mem_b_warp = transpose_b ? (K = 2, N = 64) : (K = 32, N = 4),
-                        mem_cd_warp = (M = 64, N = 1),
+                    compute_warp = (M = 16, N = 32, K = 16),
 
-                        mem_a_thread = transpose_a ? (M = 1, K = 4) : (M = 4, K = 1),
-                        mem_b_thread = transpose_b ? (K = 1, N = 4) : (K = 4, N = 1),
-                        mem_cd_thread = (M = 2, N = 1),
+                    block_shape = (M = 64, N = 64, K = 32),
 
-                        is_a_col_major = !transpose_a,
-                        is_b_col_major = !transpose_b
-                    )
+                    mem_a_warp = transpose_a ? (M = 4, K = 32) : (M = 64, K = 2),
+                    mem_b_warp = transpose_b ? (K = 2, N = 64) : (K = 32, N = 4),
+                    mem_cd_warp = (M = 64, N = 1),
 
-                trans_a = conjugate_a ? Transform.Elementwise(conj) : Transform.Elementwise()
-                trans_b = conjugate_b ? Transform.Elementwise(conj) : Transform.Elementwise()
+                    mem_a_thread = transpose_a ? (M = 1, K = 4) : (M = 4, K = 1),
+                    mem_b_thread = transpose_b ? (K = 1, N = 4) : (K = 4, N = 1),
+                    mem_cd_thread = (M = 2, N = 1),
 
-                GemmKernels.matmul(a, b, c, d, conf;
-                                transform_global_to_shared_a = trans_a,
-                                transform_global_to_shared_b = trans_b)
+                    is_a_col_major = !transpose_a,
+                    is_b_col_major = !transpose_b
+                )
 
-                new_a_h = conjugate_a ? conj.(a_h) : a_h
-                new_b_h = conjugate_b ? conj.(b_h) : b_h
+            trans_a = conjugate_a ? Transform.Elementwise(conj) : Transform.Elementwise()
+            trans_b = conjugate_b ? Transform.Elementwise(conj) : Transform.Elementwise()
 
-                # Transpose outputs, if necessary
-                new_a_h = transpose_a ? transpose(new_a_h) : new_a_h
-                new_b_h = transpose_b ? transpose(new_b_h) : new_b_h
+            GemmKernels.matmul(a, b, c, d, conf;
+                            transform_global_to_shared_a = trans_a,
+                            transform_global_to_shared_b = trans_b)
 
-                # TODO: Figure out why changing this to a * b + c = d instead of a * b = d - c
-                # makes tests fail for CC (see #19).
-                @test all(isapprox.(new_a_h * new_b_h, Array(d) - c_h; rtol=sqrt(eps(Float16))));
-            end
+            new_a_h = conjugate_a ? conj.(a_h) : a_h
+            new_b_h = conjugate_b ? conj.(b_h) : b_h
+
+            # Transpose outputs, if necessary
+            new_a_h = transpose_a ? transpose(new_a_h) : new_a_h
+            new_b_h = transpose_b ? transpose(new_b_h) : new_b_h
+
+            # TODO: Figure out why changing this to a * b + c = d instead of a * b = d - c
+            # makes tests fail for CC (see #19).
+            @test all(isapprox.(new_a_h * new_b_h, Array(d) - c_h; rtol=sqrt(eps(Float16))));
         end
     end
 
