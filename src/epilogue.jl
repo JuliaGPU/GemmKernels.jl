534bb64eec45c7beda37b6ef980e5a331b0912aa
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-08-04 15:13:30 +0200

    Add hardcoded bias

--- src/epilogue.jl
+++ src/epilogue.jl
@@ -1,3 +1,4 @@
+export Epilogue
 module Epilogue
 
 using CUDA
@@ -27,6 +28,46 @@ struct Default end
         @unroll for thread_tile = parallellise(warp_tile, Tile(MEM_CD_THREAD), laneId, 32)
             x = Layout.load(SHARED_D_LAYOUT, shmem_d, thread_tile)
             x = transform(x, thread_tile)
+            Layout.store!(GLOBAL_D_LAYOUT, d, x, translate(thread_tile, (M = block_i, N = block_j)))
+        end
+    end
+end
+
+# -------------
+# Bias epilogue
+# -------------
+
+struct Bias{B}
+    bias_vector::B
+end
+
+@inline function apply_bias(x)
+    @unroll for i = 1 : size(x, 1)
+        @unroll for j = 1 : size(x, 2)
+            @inbounds x[i, j] = ntuple(k -> VecElement{Float32}(x[i, j][k].value + 1), Val(4))
+        end
+    end
+end
+
+@inline function (ep::Bias{B})(d, shmem_d, transform, conf::Type{GemmKernels.Config{MATMUL_SHAPE, BLOCK_SHAPE, WARPS_PER_BLOCK, MEM_A_WARP, MEM_A_THREAD, MEM_B_WARP, MEM_B_THREAD, MEM_CD_WARP, MEM_CD_THREAD, COMPUTE_WARP, COMPUTE_OP_SHAPE, GLOBAL_A_LAYOUT, GLOBAL_B_LAYOUT, GLOBAL_C_LAYOUT, GLOBAL_D_LAYOUT, SHARED_A_LAYOUT, SHARED_B_LAYOUT, SHARED_C_LAYOUT, SHARED_D_LAYOUT, OPERATOR, IS_A_COL_MAJOR, IS_B_COL_MAJOR}}) where {MATMUL_SHAPE, BLOCK_SHAPE, WARPS_PER_BLOCK, MEM_A_WARP, MEM_A_THREAD, MEM_B_WARP, MEM_B_THREAD, MEM_CD_WARP, MEM_CD_THREAD, COMPUTE_WARP, COMPUTE_OP_SHAPE, GLOBAL_A_LAYOUT, GLOBAL_B_LAYOUT, GLOBAL_C_LAYOUT, GLOBAL_D_LAYOUT, SHARED_A_LAYOUT, SHARED_B_LAYOUT, SHARED_C_LAYOUT, SHARED_D_LAYOUT, OPERATOR, IS_A_COL_MAJOR, IS_B_COL_MAJOR, B}
+    # Constants
+    block_i = (blockIdx().x - 1) * BLOCK_SHAPE.M
+    block_j = (blockIdx().y - 1) * BLOCK_SHAPE.N
+
+    warpId = (threadIdx().x - 1) ÷ 32 + 1
+    laneId = (threadIdx().x - 1) % 32 + 1
+
+    gemm_sz = Tile(MATMUL_SHAPE)
+    block_tile = Tile(BLOCK_SHAPE)
+
+    # Cooperatively store a BLOCK_SHAPE.M x BLOCK_SHAPE.N tile of D from shared to global memory within one threadblock
+    @unroll for warp_tile = parallellise(block_tile.MN, Tile(MEM_CD_WARP), warpId, WARPS_PER_BLOCK)
+        @unroll for thread_tile = parallellise(warp_tile, Tile(MEM_CD_THREAD), laneId, 32)
+            x = Layout.load(SHARED_D_LAYOUT, shmem_d, thread_tile)
+            x = transform(x, thread_tile)
+
+            apply_bias(x)
+
             Layout.store!(GLOBAL_D_LAYOUT, d, x, translate(thread_tile, (M = block_i, N = block_j)))
         end
     end
---
+++ tmp.jl
@@ -0,0 +1,57 @@
+using CUDA
+using GemmKernels
+using Test
+
+function run_gemm()
+    M = 2048;
+    N = 2048;
+    K = 2048;
+
+    transpose_a = false;
+    transpose_b = false;
+
+    a_h = rand(Float16, (M, K)) / sqrt(Float16(K))
+    b_h = rand(Float16, (K, N)) / sqrt(Float16(K))
+    c_h = rand(Float32, (M, N))
+
+
+    # Transpose input if necessary
+    a_h = transpose_a ? transpose(a_h) : a_h
+    b_h = transpose_b ? transpose(b_h) : b_h
+
+
+    a   = CuArray(a_h)
+    b   = CuArray(b_h)
+    c   = CuArray(c_h)
+    d   = similar(c)
+
+    # bias vector: 1 element per column
+    # so for an M x N matrix, we need a 1 x N vector
+    #= bias = CuArray(rand(Float32, (1, N))) =#
+    bias = CuArray(ones(Float32, (1, N)));
+
+    ep = Epilogue.Bias(nothing)
+
+    conf = GemmKernels.get_config(
+                                  gemm_shape = (M = M, N = N, K = K),
+                                  operator = Operator.WMMAOp{16, 16, 16},
+                                  global_a_layout = transpose_a ? Layout.AlignedRowMajor{Float16} : Layout.AlignedColMajor{Float16},
+                                  global_b_layout = transpose_b ? Layout.AlignedRowMajor{Float16} : Layout.AlignedColMajor{Float16},
+
+                                  global_c_layout = Layout.AlignedColMajor{Float32},
+                                  global_d_layout = Layout.AlignedColMajor{Float32},
+
+                                  is_a_col_major = !transpose_a,
+                                  is_b_col_major = !transpose_b,
+                                 )
+
+    GemmKernels.matmul(a, b, c, d, conf;
+                       epilogue = ep)
+
+
+    # Transpose outputs, if necessary
+    new_a_h = transpose_a ? transpose(a_h) : a_h
+    new_b_h = transpose_b ? transpose(b_h) : b_h
+
+    @test all(isapprox.((Float32.(new_a_h) * Float32.(new_b_h) + c_h) .+ Array(bias), Array(d); rtol = sqrt(eps(Float16))))
+end
