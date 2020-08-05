081788a0d176de9ed77a0cfba04b92440264d86f
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-08-05 12:57:41 +0200

    Add epilogue to add a bias vector to the matrix output (#23)
    * Add hardcoded bias
    
    * Add part of bias epilogue
    
    * Make bias epilogue work
    
    * Generalise bias epilogue
    
    * Reorder and rename apply_bias
    
    * Add test for bias
    
    * Remove activation function from test
    
    * Remove temporary file

--- src/epilogue.jl
+++ src/epilogue.jl
@@ -1,3 +1,4 @@
+export Epilogue
 module Epilogue
 
 using CUDA
@@ -26,6 +27,50 @@ struct Default end
     @unroll for warp_tile = parallellise(block_tile.MN, Tile(MEM_CD_WARP), warpId, WARPS_PER_BLOCK)
         @unroll for thread_tile = parallellise(warp_tile, Tile(MEM_CD_THREAD), laneId, 32)
             x = Layout.load(SHARED_D_LAYOUT, shmem_d, thread_tile)
+            x = transform(x, thread_tile)
+            Layout.store!(GLOBAL_D_LAYOUT, d, x, translate(thread_tile, (M = block_i, N = block_j)))
+        end
+    end
+end
+
+# -------------
+# Bias epilogue
+# -------------
+
+struct Bias{B}
+    bias_pointer::B
+end
+
+@inline function apply_bias!(x, bias_pointer::CuPtr{Float32}, thread_tile)
+    dev_ptr = reinterpret(Core.LLVMPtr{Float32, AS.Global}, bias_pointer)
+
+    @unroll for i = 1 : size(x, 1)
+        @unroll for j = 1 : size(x, 2)
+            # Load bias value for this column
+            col = thread_tile.index.N + j
+            b = unsafe_load(dev_ptr, col)
+
+            @inbounds x[i, j] = ntuple(k -> VecElement{Float32}(x[i, j][k].value + b), Val(4))
+        end
+    end
+end
+
+@inline function (ep::Bias{B})(d, shmem_d, transform, conf::Type{GemmKernels.Config{MATMUL_SHAPE, BLOCK_SHAPE, WARPS_PER_BLOCK, MEM_A_WARP, MEM_A_THREAD, MEM_B_WARP, MEM_B_THREAD, MEM_CD_WARP, MEM_CD_THREAD, COMPUTE_WARP, COMPUTE_OP_SHAPE, GLOBAL_A_LAYOUT, GLOBAL_B_LAYOUT, GLOBAL_C_LAYOUT, GLOBAL_D_LAYOUT, SHARED_A_LAYOUT, SHARED_B_LAYOUT, SHARED_C_LAYOUT, SHARED_D_LAYOUT, OPERATOR, IS_A_COL_MAJOR, IS_B_COL_MAJOR}}) where {MATMUL_SHAPE, BLOCK_SHAPE, WARPS_PER_BLOCK, MEM_A_WARP, MEM_A_THREAD, MEM_B_WARP, MEM_B_THREAD, MEM_CD_WARP, MEM_CD_THREAD, COMPUTE_WARP, COMPUTE_OP_SHAPE, GLOBAL_A_LAYOUT, GLOBAL_B_LAYOUT, GLOBAL_C_LAYOUT, GLOBAL_D_LAYOUT, SHARED_A_LAYOUT, SHARED_B_LAYOUT, SHARED_C_LAYOUT, SHARED_D_LAYOUT, OPERATOR, IS_A_COL_MAJOR, IS_B_COL_MAJOR, B}
+    # Constants
+    block_i = (blockIdx().x - 1) * BLOCK_SHAPE.M
+    block_j = (blockIdx().y - 1) * BLOCK_SHAPE.N
+
+    warpId = (threadIdx().x - 1) ÷ 32 + 1
+    laneId = (threadIdx().x - 1) % 32 + 1
+
+    gemm_sz = Tile(MATMUL_SHAPE)
+    block_tile = Tile(BLOCK_SHAPE)
+
+    # Cooperatively store a BLOCK_SHAPE.M x BLOCK_SHAPE.N tile of D from shared to global memory within one threadblock
+    @unroll for warp_tile = parallellise(block_tile.MN, Tile(MEM_CD_WARP), warpId, WARPS_PER_BLOCK)
+        @unroll for thread_tile = parallellise(warp_tile, Tile(MEM_CD_THREAD), laneId, 32)
+            x = Layout.load(SHARED_D_LAYOUT, shmem_d, thread_tile)
+            apply_bias!(x, ep.bias_pointer, translate(thread_tile, (M = block_i, N = block_j)))
             x = transform(x, thread_tile)
             Layout.store!(GLOBAL_D_LAYOUT, d, x, translate(thread_tile, (M = block_i, N = block_j)))
         end
