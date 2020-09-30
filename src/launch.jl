1114787f2825687e5346d83c4c08abcd77b2febb
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-09-30 16:47:43 +0200

    Allow choosing kernel

--- src/launch.jl
+++ src/launch.jl
@@ -9,7 +9,8 @@ function matmul(a, b, c, d, conf;
                 transform_shared_to_regs_b = Transform.Elementwise(),
                 transform_shared_to_regs_c = Transform.Elementwise(),
                 transform_regs_to_shared_d = Transform.Elementwise(),
-                epilogue = Epilogue.Default())
+                epilogue = Epilogue.Default(),
+                kernel_fun = Kernel.matmul_singlestage)
 
     args = [a, b, c, d,
             transform_global_to_shared_a, transform_global_to_shared_b, transform_global_to_shared_c, transform_shared_to_global_d,
@@ -20,7 +21,7 @@ function matmul(a, b, c, d, conf;
     GC.@preserve args begin
         kernel_args = cudaconvert.(args)
         kernel_tt = Tuple{Core.Typeof.(kernel_args)...}
-        kernel = cufunction(Kernel.matmul_impl, kernel_tt; )
+        kernel = cufunction(kernel_fun, kernel_tt; )
         attributes(kernel.fun)[CUDA.FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES] = 64 * 1024
         kernel(kernel_args...; conf.launch_args...)
     end
