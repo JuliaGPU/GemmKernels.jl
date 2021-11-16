21724fcde0850b89d30e5a50fe8ec6fc21688fe4
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2021-11-16 20:00:24 +0100

    Make layouts abstract types to allow subtyping

--- src/layout.jl
+++ src/layout.jl
@@ -49,29 +49,29 @@ abstract type LayoutBase{T} end
 # Padded layouts
 # --------------
 
-struct Padded{L, P} end
+abstract type Padded{L, P} end
 
-@inline function pad_logical_coord(::Type{Padded{L, P}}, crd::NamedTuple) where {L, P}
+@inline function pad_logical_coord(::Type{<:Padded{L, P}}, crd::NamedTuple) where {L, P}
     t = Tuple(crd)
     return typeof(crd)((Base.first(t) + P, Base.tail(t)...))
 end
 
-@inline eltype(::Type{Padded{L, P}}) where {L, P} = eltype(L)
-@inline physical_size(::Type{Padded{L, P}}, logical_size::NamedTuple) where {L, P} = physical_size(L, pad_logical_coord(Padded{L, P}, logical_size))
-@inline load(::Type{Padded{L, P}}, workspace, tile::Tile, logical_size::NamedTuple) where {L, P} = load(L, workspace, tile)
-@inline store!(::Type{Padded{L, P}}, workspace, value, tile::Tile) where {L, P} = store!(L, workspace, value, tile::Tile)
+@inline eltype(::Type{<:Padded{L, P}}) where {L, P} = eltype(L)
+@inline physical_size(::Type{<:Padded{L, P}}, logical_size::NamedTuple) where {L, P} = physical_size(L, pad_logical_coord(Padded{L, P}, logical_size))
+@inline load(::Type{<:Padded{L, P}}, workspace, tile::Tile, logical_size::NamedTuple) where {L, P} = load(L, workspace, tile)
+@inline store!(::Type{<:Padded{L, P}}, workspace, value, tile::Tile) where {L, P} = store!(L, workspace, value, tile::Tile)
 
 # ---------------
 # AlignedColMajor
 # ---------------
 
-struct AlignedColMajor{T} <: LayoutBase{T} end
+abstract type AlignedColMajor{T} <: LayoutBase{T} end
 
-@inline physical_size(::Type{Padded{AlignedColMajor{T}, P}}, logical_size::NamedTuple) where {T, P} = (logical_size[1] + P, logical_size[2])
+@inline physical_size(::Type{<:Padded{AlignedColMajor{T}, P}}, logical_size::NamedTuple) where {T, P} = (logical_size[1] + P, logical_size[2])
 
-@inline fragtype(::Type{AlignedColMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{16 ÷ sizeof(T), VecElement{T}}
+@inline fragtype(::Type{<:AlignedColMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{16 ÷ sizeof(T), VecElement{T}}
 
-@inline function load(::Type{AlignedColMajor{T}}, workspace, tile::Tile{size}) where {T, size}
+@inline function load(::Type{<:AlignedColMajor{T}}, workspace, tile::Tile{size}) where {T, size}
     N = 16 ÷ sizeof(T)
 
     linear_base = linearise(tile.base, Base.size(workspace))
@@ -80,7 +80,7 @@ struct AlignedColMajor{T} <: LayoutBase{T} end
     return vloada(Vec{N, T}, pointer(workspace), linear_base + linear_offset - 1)
 end
 
-@inline function store!(::Type{AlignedColMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
+@inline function store!(::Type{<:AlignedColMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
     N = 16 ÷ sizeof(T)
 
     linear_base = linearise(tile.base, Base.size(workspace))
@@ -93,9 +93,9 @@ end
 # Diagonal
 # --------
 
-struct Diagonal{T} <: LayoutBase{T} end
+abstract type Diagonal{T} <: LayoutBase{T} end
 
-@inline function load(::Type{Diagonal{T}}, workspace, tile::Tile{size}) where {T, size}
+@inline function load(::Type{<:Diagonal{T}}, workspace, tile::Tile{size}) where {T, size}
     N = 16 ÷ sizeof(T)
 
     # The row index is given by t.index[1] + (k - 1), the column index is given by t.index[2] (0-based).
@@ -104,19 +104,19 @@ struct Diagonal{T} <: LayoutBase{T} end
     return ntuple(k -> VecElement{Float16}(tile.index[1] + k - 1 == tile.index[2] ? @inbounds(workspace[tile.index[2] + 1]) : 0), Val(8))
 end
 
-@inline threadblock_condition(layout_a::Type{Diagonal{T}}, layout_b, block_i, block_j, block_k, block_tile) where {T} = abs(block_i - block_k) <= block_tile.size.K
+@inline threadblock_condition(layout_a::Type{<:Diagonal{T}}, layout_b, block_i, block_j, block_k, block_tile) where {T} = abs(block_i - block_k) <= block_tile.size.K
 
 # ---------------
 # AlignedRowMajor
 # ---------------
 
-struct AlignedRowMajor{T} <: LayoutBase{T} end
+abstract type AlignedRowMajor{T} <: LayoutBase{T} end
 
-@inline physical_size(::Type{Padded{AlignedRowMajor{T}, P}}, logical_size::NamedTuple) where {T, P} = (logical_size[2] + P, logical_size[1])
+@inline physical_size(::Type{<:Padded{AlignedRowMajor{T}, P}}, logical_size::NamedTuple) where {T, P} = (logical_size[2] + P, logical_size[1])
 
-@inline fragtype(::Type{AlignedRowMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{16 ÷ sizeof(T), VecElement{T}}
+@inline fragtype(::Type{<:AlignedRowMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{16 ÷ sizeof(T), VecElement{T}}
 
-@inline function load(::Type{AlignedRowMajor{T}}, workspace, tile::Tile{size}) where {T, size}
+@inline function load(::Type{<:AlignedRowMajor{T}}, workspace, tile::Tile{size}) where {T, size}
     N = 16 ÷ sizeof(T)
 
     linear_base = linearise(reverse(Tuple(tile.base)), Base.size(workspace))
@@ -125,7 +125,7 @@ struct AlignedRowMajor{T} <: LayoutBase{T} end
     return vloada(Vec{N, T}, pointer(workspace), linear_base + linear_offset - 1)
 end
 
-@inline function store!(::Type{AlignedRowMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
+@inline function store!(::Type{<:AlignedRowMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
     N = 16 ÷ sizeof(T)
 
     linear_base = linearise(reverse(Tuple(tile.base)), Base.size(workspace))
@@ -138,11 +138,11 @@ end
 # InterleavedColMajor
 # -------------------
 
-struct InterleavedColMajor{T} <: LayoutBase{T} end
+abstract type InterleavedColMajor{T} <: LayoutBase{T} end
 
-@inline fragtype(::Type{InterleavedColMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
+@inline fragtype(::Type{<:InterleavedColMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
 
-@inline function load(::Type{InterleavedColMajor{T}}, workspace, tile::Tile{size}) where {T, size}
+@inline function load(::Type{<:InterleavedColMajor{T}}, workspace, tile::Tile{size}) where {T, size}
     x = ntuple(i -> zero(Complex{T}), tile.size[1] * tile.size[2])
 
     @unroll for j = 1 : tile.size[2]
@@ -156,7 +156,7 @@ struct InterleavedColMajor{T} <: LayoutBase{T} end
     return x
 end
 
-@inline function store!(::Type{InterleavedColMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
+@inline function store!(::Type{<:InterleavedColMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
     @unroll for j = 1 : tile.size[2]
         @unroll for i = 1 : tile.size[1]
             t = translate_offset(tile, (i - 1, j - 1))
@@ -170,11 +170,11 @@ end
 # InterleavedRowMajor
 # -------------------
 
-struct InterleavedRowMajor{T} <: LayoutBase{T} end
+abstract type InterleavedRowMajor{T} <: LayoutBase{T} end
 
-@inline fragtype(::Type{InterleavedRowMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
+@inline fragtype(::Type{<:InterleavedRowMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
 
-@inline function load(::Type{InterleavedRowMajor{T}}, workspace, tile::Tile{size}) where {T, size}
+@inline function load(::Type{<:InterleavedRowMajor{T}}, workspace, tile::Tile{size}) where {T, size}
     x = ntuple(i -> zero(Complex{T}), tile.size[1] * tile.size[2])
 
     @unroll for i = 1 : tile.size[1]
@@ -188,7 +188,7 @@ struct InterleavedRowMajor{T} <: LayoutBase{T} end
     return x
 end
 
-@inline function store!(::Type{InterleavedRowMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
+@inline function store!(::Type{<:InterleavedRowMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
     @unroll for i = 1 : tile.size[1]
         @unroll for j = 1 : tile.size[2]
             t = translate_offset(tile, (i - 1, j - 1))
@@ -202,16 +202,16 @@ end
 # SplitColMajor
 # -------------
 
-struct SplitColMajor{T} <: LayoutBase{T} end
+abstract type SplitColMajor{T} <: LayoutBase{T} end
 
-@inline function physical_size(::Type{SplitColMajor{T}}, logical_size::NamedTuple) where {T}
+@inline function physical_size(::Type{<:SplitColMajor{T}}, logical_size::NamedTuple) where {T}
     t = Tuple(logical_size)
     return (t..., 2)
 end
 
-@inline fragtype(::Type{SplitColMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
+@inline fragtype(::Type{<:SplitColMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
 
-@inline function load(::Type{SplitColMajor{T}}, workspace, tile::Tile{size}) where {T, size}
+@inline function load(::Type{<:SplitColMajor{T}}, workspace, tile::Tile{size}) where {T, size}
     x = ntuple(i -> zero(Complex{T}), tile.size[1] * tile.size[2])
 
     @unroll for j = 1 : tile.size[2]
@@ -226,7 +226,7 @@ end
     return x
 end
 
-@inline function store!(::Type{SplitColMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
+@inline function store!(::Type{<:SplitColMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
     @unroll for j = 1 : tile.size[2]
         @unroll for i = 1 : tile.size[1]
             t = translate_offset(tile, (i - 1, j - 1))
@@ -241,15 +241,15 @@ end
 # SplitRowMajor
 # -------------
 
-struct SplitRowMajor{T} <: LayoutBase{T} end
+abstract type SplitRowMajor{T} <: LayoutBase{T} end
 
-@inline function physical_size(::Type{Padded{SplitRowMajor{T}, P}}, logical_size::NamedTuple) where {T, P}
+@inline function physical_size(::Type{<:Padded{SplitRowMajor{T}, P}}, logical_size::NamedTuple) where {T, P}
     return (logical_size[2] + P, logical_size[1], 2)
 end
 
-@inline fragtype(::Type{SplitRowMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
+@inline fragtype(::Type{<:SplitRowMajor{T}}, tile_size::NamedTuple) where {T} = NTuple{tile_size[1] * tile_size[2], Complex{T}}
 
-@inline function load(::Type{SplitRowMajor{T}}, workspace, tile::Tile{size}) where {T, size}
+@inline function load(::Type{<:SplitRowMajor{T}}, workspace, tile::Tile{size}) where {T, size}
     x = ntuple(i -> zero(Complex{T}), tile.size[1] * tile.size[2])
 
     @unroll for i = 1 : tile.size[1]
@@ -264,7 +264,7 @@ end
     return x
 end
 
-@inline function store!(::Type{SplitRowMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
+@inline function store!(::Type{<:SplitRowMajor{T}}, workspace, value, tile::Tile{size}) where {T, size}
     @unroll for i = 1 : tile.size[1]
         @unroll for j = 1 : tile.size[2]
             t = translate_offset(tile, (i - 1, j - 1))
