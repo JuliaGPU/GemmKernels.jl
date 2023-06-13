5a32e463437b0c16eda2c07791560c57eb144cc2
wardvermeulen <wardvermeulen@telenet.be>
2023-06-13 17:52:44 +0200

    variable name refactor

--- src/tensors/gettlayout.jl
+++ src/tensors/gettlayout.jl
@@ -5,82 +5,81 @@ using GemmKernels.Layout
 using GemmKernels.Tiling
 using KernelAbstractions.Extras: @unroll
 
-@inline function sloada(::Type{Layout.Vec{NUMEL, T}}, workspace, offset::Int, strided_over_size::Int) where {NUMEL, T}
+@inline function sloada(::Type{Layout.Vec{NUMEL, T}}, workspace, offset::Int, strideOverExtent::Int) where {NUMEL, T}
     return ntuple(Val(NUMEL)) do i
-        @inbounds VecElement{T}(workspace[offset + (i - 1) * strided_over_size])
+        @inbounds VecElement{T}(workspace[offset + (i - 1) * strideOverExtent])
     end
 end
 
-@inline function sstorea!(::Type{Layout.Vec{NUMEL, T}}, workspace, val, offset::Int, strided_over_size::Int) where {NUMEL, T}
+@inline function sstorea!(::Type{Layout.Vec{NUMEL, T}}, workspace, val, offset::Int, strideOverExtent::Int) where {NUMEL, T}
     for i in 1 : NUMEL
-        @inbounds workspace[offset + (i - 1) * strided_over_size] = val[i].value
+        @inbounds workspace[offset + (i - 1) * strideOverExtent] = val[i].value
     end
 end
 
-abstract type GETTLayoutColMajor{T, TM_strides, TM_div, GM_mul, TK_strides, TK_div, GK_mul, T_mod, is_load_strided, strided_over_size} <: Layout.AlignedColMajor{T} end
+abstract type GETTLayoutColMajor{T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent} <: Layout.AlignedColMajor{T} end
 
-abstract type GETTLayoutRowMajor{T, TM_strides, TM_div, GM_mul, TK_strides, TK_div, GK_mul, T_mod, is_load_strided, strided_over_size} <: Layout.AlignedRowMajor{T} end
+abstract type GETTLayoutRowMajor{T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent} <: Layout.AlignedRowMajor{T} end
 
 @inline function Layout.load(
         ::Union{
-            Type{GETTLayoutColMajor{T, TM_strides, TM_div, GM_mul, TK_strides, TK_div, GK_mul, T_mod, is_load_strided, strided_over_size}},
-            Type{GETTLayoutRowMajor{T, TM_strides, TM_div, GM_mul, TK_strides, TK_div, GK_mul, T_mod, is_load_strided, strided_over_size}}
+            Type{GETTLayoutColMajor{T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent}},
+            Type{GETTLayoutRowMajor{T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent}}
         }, workspace, tile::Tile{size}
-    ) where {T, TM_strides, TM_div, GM_mul, TK_strides, TK_div, GK_mul, T_mod, is_load_strided, strided_over_size, size}
+    ) where {T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent, size}
 
     NUMEL = 16 ÷ sizeof(T)
 
-    M = tile.base[1] + tile.offset[1]
-    K = tile.base[2] + tile.offset[2]
+    G1 = tile.base[1] + tile.offset[1]
+    G2 = tile.base[2] + tile.offset[2]
 
     offset = 1
 
-    @unroll for i in eachindex(TM_strides)
-        stride_offset = (M ÷ TM_div[i]) % T_mod[TM_strides[i]]
-        offset += stride_offset * GM_mul[i]
+    @unroll for i in eachindex(dimensionsT1)
+        stride_offset = (G1 ÷ divT1[i]) % modT[dimensionsT1[i]]
+        offset += stride_offset * mulG1[i]
     end
 
-    @unroll for i in eachindex(TK_strides)
-        stride_offset = (K ÷ TK_div[i]) % T_mod[TK_strides[i]]
-        offset += stride_offset * GK_mul[i]
+    @unroll for i in eachindex(dimensionsT2)
+        stride_offset = (G2 ÷ divT2[i]) % modT[dimensionsT2[i]]
+        offset += stride_offset * mulG2[i]
     end
 
-    if (is_load_strided == false)
+    if (isLoadOrStoreStrided == false)
         return Layout.vloada(Layout.Vec{NUMEL, T}, pointer(workspace), offset)
     else
-        return GETTLayout.sloada(Layout.Vec{NUMEL, T}, workspace, offset, strided_over_size)
+        return GETTLayout.sloada(Layout.Vec{NUMEL, T}, workspace, offset, strideOverExtent)
     end
 end
 
 @inline function Layout.store!(
         ::Union{
-            Type{GETTLayoutColMajor{T, TM_strides, TM_div, GM_mul, TN_strides, TN_div, GN_mul, T_mod, is_store_strided, strided_over_size}},
-            Type{GETTLayoutRowMajor{T, TM_strides, TM_div, GM_mul, TN_strides, TN_div, GN_mul, T_mod, is_store_strided, strided_over_size}}
+            Type{GETTLayoutColMajor{T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent}},
+            Type{GETTLayoutRowMajor{T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent}}
         }, workspace, value, tile::Tile{size}
-    ) where {T, TM_strides, TM_div, GM_mul, TN_strides, TN_div, GN_mul, T_mod, is_store_strided, strided_over_size, size}
+    ) where {T, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent, size}
 
     NUMEL = 16 ÷ sizeof(T)
 
-    M = tile.base.M + tile.offset.M
-    N = tile.base.N + tile.offset.N
+    G1 = tile.base[1] + tile.offset.[1]
+    G2 = tile.base[2] + tile.offset.[2]
 
     offset = 1
 
-    @unroll for i in eachindex(TM_strides)
-        stride_offset = (M ÷ TM_div[i]) % T_mod[TM_strides[i]]
-        offset += stride_offset * GM_mul[i]
+    @unroll for i in eachindex(dimensionsT1)
+        stride_offset = (G1 ÷ divT1[i]) % modT[dimensionsT1[i]]
+        offset += stride_offset * mulG1[i]
     end
 
-    @unroll for i in eachindex(TN_strides)
-        stride_offset = (N ÷ TN_div[i]) % T_mod[TN_strides[i]]
-
-        offset += stride_offset * GN_mul[i]
+    @unroll for i in eachindex(dimensionsT2)
+        stride_offset = (G2 ÷ divT2[i]) % modT[dimensionsT2[i]]
+        offset += stride_offset * mulG2[i]
     end
 
-    if (is_store_strided == false)
+    if (isLoadOrStoreStrided == false)
         return Layout.vstorea!(Layout.Vec{NUMEL, T}, pointer(workspace), value, offset)
     else
-        GETTLayout.sstorea!(Layout.Vec{NUMEL, T}, workspace, value, offset, strided_over_size)
+        GETTLayout.sstorea!(Layout.Vec{NUMEL, T}, workspace, value, offset, strideOverExtent)
     end
 end
 
@@ -91,10 +90,10 @@ end
 
 export precomputeGETTLayoutConstants
 function precomputeGETTLayoutConstants(
-    T_strides_sizes::Vector{Int},
-    T_strides::Tuple{Vector{Int}, Vector{Int}},
-    is_load_or_store_strided::Bool,
-    load_or_store_strided_over::Union{Vector{Int}, Nothing} = nothing,
+    extent::Vector{Int},
+    dimensions::Tuple{Vector{Int}, Vector{Int}},
+    isLoadOrStoreStrided::Bool,
+    strideOver::Union{Vector{Int}, Nothing} = nothing,
 )
     # 1. Convert the tensor strides from Tuple{Vector{Int}, Vector{int}}  to two separate 
     # Tuple{Int, Int, ...}.
@@ -102,99 +101,99 @@ function precomputeGETTLayoutConstants(
     # → For the A matrix this will contain the tensor strides corresponding to the M stride.
     # e.g. for D[A, B, C] = A[B, D, A] * B[D, C] this will be (1, 3), since B and A belong to the
     # M stride.
-    T1_strides = Tuple(x for x in T_strides[1])
+    dimensionsT1 = Tuple(x for x in dimensions[1])
     # → analogous, T2_stride will be equal to (2,) for the above example, since D belongs to the
     # K stride.
-    T2_strides = Tuple(x for x in T_strides[2])
+    dimensionsT2 = Tuple(x for x in dimensions[2])
 
 
     # 2. Precompute the divisors used to calculate the tensor stride offsets.
-    # → T1_stride_offset = (M ÷ T1_div[i]) % T1_mod[i] 
-    T1_div = Vector{Int}(undef, length(T1_strides))
+    # → T1_stride_offset = (M ÷ divT1[i]) % T1_mod[i] 
+    divT1 = Vector{Int}(undef, length(dimensionsT1))
     div = 1
-    for (idx, stride_idx) in enumerate(T1_strides)
-        T1_div[idx] = div
-        div *= T_strides_sizes[stride_idx]
+    for (idx, stride_idx) in enumerate(dimensionsT1)
+        divT1[idx] = div
+        div *= extent[stride_idx]
     end
-    T1_div = Tuple(x for x in T1_div)
+    divT1 = Tuple(x for x in divT1)
 
     # 2b. Do the same for T2_stride.
-    T2_div = Vector{Int}(undef, length(T2_strides))
+    divT2 = Vector{Int}(undef, length(dimensionsT2))
     div = 1
-    for (idx, stride_idx) in enumerate(T2_strides)
-        T2_div[idx] = div
-        div *= T_strides_sizes[stride_idx]
+    for (idx, stride_idx) in enumerate(dimensionsT2)
+        divT2[idx] = div
+        div *= extent[stride_idx]
     end
-    T2_div = Tuple(x for x in T2_div)
+    divT2 = Tuple(x for x in divT2)
 
 
     # 3. Precompute the moduli used to calculate the tensor stride offsets.
-    # These are simply the sizes of the tensor strides. Again, converted to Tuple{Int, Int, ...}.
-    T_mod = Tuple(x for x in T_strides_sizes)
+    # These are simply the extents of the tensor. Again, converted to Tuple{Int, Int, ...}.
+    modT = Tuple(x for x in extent)
 
 
     # 4. Precompute the multiplicative terms used to calculate the GEMM stride offsets.
-    # → offset += T1_stride_offset * G1_mul[i]
-    G1_mul = Vector{Int}(undef, length(T1_strides))
-    for (idx, stride_idx) in enumerate(T1_strides)
-        G1_mul[idx] = 1
+    # → offset += T1_stride_offset * mulG1[i]
+    mulG1 = Vector{Int}(undef, length(dimensionsT1))
+    for (idx, stride_idx) in enumerate(dimensionsT1)
+        mulG1[idx] = 1
         for j = 1 : (stride_idx - 1) 
-            G1_mul[idx] *= T_strides_sizes[j]
+            mulG1[idx] *= extent[j]
         end
     end
-    G1_mul = Tuple(x for x in G1_mul)
+    mulG1 = Tuple(x for x in mulG1)
 
-    # 4b. Do the same for G2_mul.
-    G2_mul = Vector{Int}(undef, length(T2_strides))
-    for (idx, stride_idx) in enumerate(T2_strides)
-        G2_mul[idx] = 1
+    # 4b. Do the same for mulG2.
+    mulG2 = Vector{Int}(undef, length(dimensionsT2))
+    for (idx, stride_idx) in enumerate(dimensionsT2)
+        mulG2[idx] = 1
         for j = 1 : (stride_idx - 1) 
-            G2_mul[idx] *= T_strides_sizes[j]
+            mulG2[idx] *= extent[j]
         end
     end
-    G2_mul = Tuple(x for x in G2_mul)
+    mulG2 = Tuple(x for x in mulG2)
 
     # 5. Convert the Bool to an Int.
-    is_load_or_store_strided = Int(is_load_or_store_strided)
+    isLoadOrStoreStrided = Int(isLoadOrStoreStrided)
 
     # 5.b If the load or store is strided, then precompute the size of the dimensions to stride 
     # over.
-    strided_over_size = 1
-    if (isnothing(load_or_store_strided_over) == false && is_load_or_store_strided == true)
-        for stride_idx in load_or_store_strided_over
-            strided_over_size *= T_strides_sizes[stride_idx]
+    strideOverExtent = 1
+    if (isnothing(strideOver) == false && isLoadOrStoreStrided == true)
+        for stride_idx in strideOver
+            strideOverExtent *= extent[stride_idx]
         end
     end
 
     return (
-        T1_strides, T2_strides,
-        T1_div, T2_div,
-        T_mod,
-        G1_mul, G2_mul,
-        is_load_or_store_strided, strided_over_size,
+        dimensionsT1, dimensionsT2,
+        divT1, divT2,
+        modT,
+        mulG1, mulG2,
+        isLoadOrStoreStrided, strideOverExtent,
     )
 end
 
 function createGETTLayout(
     DT::DataType,
     extent::Vector{Int},
-    stride_indices::Tuple{Vector{Int}, Vector{Int}},
-    is_col_major::Bool,
-    is_load_strided::Bool,
-    load_or_store_strided_over::Union{Vector{Int}, Nothing} = nothing,
+    dimensions::Tuple{Vector{Int}, Vector{Int}},
+    isColMajor::Bool,
+    isLoadOrStoreStrided::Bool,
+    strideOver::Union{Vector{Int}, Nothing} = nothing,
 )
     (
-        T1_strides, T2_strides,
-        T1_div, T2_div,
-        T_mod,
-        G1_mul, G2_mul,
-        is_load_or_store_strided, strided_over_size
-    ) = precomputeGETTLayoutConstants(extent, stride_indices, is_load_strided, load_or_store_strided_over)
+        dimensionsT1, dimensionsT2,
+        divT1, divT2,
+        modT,
+        mulG1, mulG2,
+        isLoadOrStoreStrided, strideOverExtent
+    ) = precomputeGETTLayoutConstants(extent, dimensions, isLoadOrStoreStrided, strideOver)
 
-    if (is_col_major == true)
-        return GETTLayoutColMajor{DT, T1_strides, T1_div, G1_mul, T2_strides, T2_div, G2_mul, T_mod, is_load_or_store_strided, strided_over_size}
+    if (isColMajor == true)
+        return GETTLayoutColMajor{DT, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent}
     else
-        return GETTLayoutRowMajor{DT, T1_strides, T1_div, G1_mul, T2_strides, T2_div, G2_mul, T_mod, is_load_or_store_strided, strided_over_size}
+        return GETTLayoutRowMajor{DT, dimensionsT1, divT1, mulG1, dimensionsT2, divT2, mulG2, modT, isLoadOrStoreStrided, strideOverExtent}
     end
 end
 
