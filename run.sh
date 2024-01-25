a76e7ce1533ee7e67c27eaefa937f99c1c1c47fe
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2024-01-25 14:35:36 +0100

    Bump number of processes

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ set -Eeuo pipefail
 julia --project -e 'using Pkg; Pkg.instantiate(); Pkg.precompile();'
 julia --project=benchmarks -e 'using Pkg; Pkg.instantiate(); Pkg.precompile();'
 
-julia -p 32 --project=benchmarks -e '
+julia -p 48 --project=benchmarks -e '
     @everywhere push!(LOAD_PATH, @__DIR__)
     @everywhere include("benchmarks/runbenchmarks.jl")'
