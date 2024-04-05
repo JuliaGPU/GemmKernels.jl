55ee2070b72ff3938f4b03c73bd4bac578959293
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2024-04-05 15:01:28 +0200

    Fix

--- profile.sh
+++ profile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-rm prof*.ncu-rep
+rm -f prof*.ncu-rep
 
 for TC in 1.2-1.3-3.2 1.2.3.4-5.3-1.2.5.4; do
     for impl in cutensor gemmkernels-optimal-for-jupiter gemmkernels-optimal-for-ripper; do
