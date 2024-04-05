4afa16d9d408d2b7a6c2e362bafddcf80bbf9759
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2024-04-05 15:51:32 +0200

    Collect all metrics

--- profile.sh
+++ profile.sh
@@ -5,6 +5,6 @@ rm -f prof*.ncu-rep
 
 for TC in 1.2-1.3-3.2 1.2.3.4-5.3-1.2.5.4; do
     for impl in cutensor gemmkernels-optimal-for-jupiter gemmkernels-optimal-for-ripper; do
-        /usr/local/cuda-12.4/bin/ncu -f --profile-from-start off -o prof-host-$(hostname)-tc-$TC-impl-$impl julia --project=tuning ./profile.jl $TC $impl
+        /usr/local/cuda-12.4/bin/ncu --set full -f --profile-from-start off -o prof-host-$(hostname)-tc-$TC-impl-$impl julia --project=tuning ./profile.jl $TC $impl
     done
 done
