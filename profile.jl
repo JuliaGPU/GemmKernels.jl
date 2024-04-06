7230c9fbb288a605a83072b55816603395f49d52
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2024-04-06 15:11:19 +0200

    Profile all TCs

--- profile.jl
+++ profile.jl
@@ -1,4 +1,5 @@
 using GemmKernels, CUDA
+using DataFrames, Serialization
 
 include("configs/configs.jl")
 
@@ -7,88 +8,47 @@ const compute_type = Float16
 const accumulate_type = Float32
 const zero_c = true
 
-const EXTENTS = Dict(
-    "1.2-1.3-3.2" => [5136, 5120, 5136], # TC 12
-    "1.2.3.4-5.3-1.2.5.4" => [72, 72, 72, 72, 72], # TC 11
-)
+function get_extents(tc)
+    df = open("2024-03-28-best-configs-ripper.bin") do io
+        deserialize(io)
+    end
 
-# key: (tensor_contraction, machine).
-const OPTIMAL_PARAMS = Dict(
-    # Optimal parameters of TC 12 from sweep 2024-03-28 on ripper.
-    ("1.2-1.3-3.2", "ripper") => Dict(
-        "BLOCK_M"        => 64,
-        "BLOCK_N"        => 256,
-        "BLOCK_K"        => 32,
-        "WARPS_M"        => 2,
-        "WARPS_N"        => 4,
-        "OP_M"           => 32,
-        "OP_N"           => 8,
-        "OP_K"           => 16,
-        "kernel"         => Kernel.matmul_pipelined,
-        "is_A_col_major" => false,
-        "is_B_col_major" => true,
-        "is_D_col_major" => true,
-        "PERM_M"         => [1],
-        "PERM_N"         => [2],
-        "PERM_K"         => [3],
-    ),
+    first(filter(row -> row.name == tc, df)).extents
+end
 
-    # Optimal parameters of TC 12 from sweep 2024-03-05 on jupiter.
-    ("1.2-1.3-3.2", "jupiter") => Dict(
-        "BLOCK_M"        => 64,
-        "BLOCK_N"        => 64,
-        "BLOCK_K"        => 32,
-        "WARPS_M"        => 1,
-        "WARPS_N"        => 2,
-        "OP_M"           => 8,
-        "OP_N"           => 32,
-        "OP_K"           => 16,
-        "kernel"         => Kernel.matmul_pipelined,
-        "is_A_col_major" => nothing,
-        "is_B_col_major" => nothing,
-        "is_D_col_major" => nothing,
-        "PERM_M"         => nothing,
-        "PERM_N"         => nothing,
-        "PERM_K"         => nothing,
-    ),
+function get_optimal_params(tc, machine)
+    file = Dict("ripper" => "2024-03-28-best-configs-ripper.bin",
+                "jupiter" => "2024-03-05-best-configs-jupiter.bin")[machine]
 
-    # Optimal parameters of TC 11 from sweep 2024-03-28 on ripper.
-    ("1.2.3.4-5.3-1.2.5.4", "ripper") => Dict(
-        "BLOCK_M"        => 64,
-        "BLOCK_N"        => 64,
-        "BLOCK_K"        => 64,
-        "WARPS_M"        => 8,
-        "WARPS_N"        => 1,
-        "OP_M"           => 8,
-        "OP_N"           => 32,
-        "OP_K"           => 16,
-        "kernel"         => Kernel.matmul_pipelined,
-        "is_A_col_major" => false,
-        "is_B_col_major" => false,
-        "is_D_col_major" => true,
-        "PERM_M"         => [3],
-        "PERM_N"         => [1, 2, 4],
-        "PERM_K"         => [5],
-    ),
+    df = open(file) do io
+        deserialize(io)
+    end
 
-    ("1.2.3.4-5.3-1.2.5.4", "jupiter") => Dict(
-        "BLOCK_M"        => 64,
-        "BLOCK_N"        => 64,
-        "BLOCK_K"        => 64,
-        "WARPS_M"        => 4,
-        "WARPS_N"        => 2,
-        "OP_M"           => 16,
-        "OP_N"           => 16,
-        "OP_K"           => 16,
-        "kernel"         => Kernel.matmul_pipelined,
-        "is_A_col_major" => nothing,
-        "is_B_col_major" => nothing,
-        "is_D_col_major" => nothing,
-        "PERM_M"         => nothing,
-        "PERM_N"         => nothing,
-        "PERM_K"         => nothing,
-    )
-)
+    if in("name", names(df))
+        row = first(filter(row -> row.name == tc, df))
+    else
+        row = first(filter(row -> row.parseable_name == tc, df))
+    end
+
+    has_layout_sweep = in("is_A_col_major", names(df))
+
+    Dict(
+        "BLOCK_M" => row.BLOCK_M,
+        "BLOCK_N" => row.BLOCK_N,
+        "BLOCK_K" => row.BLOCK_K,
+        "WARPS_M" => row.WARPS_M,
+        "WARPS_N" => row.WARPS_N,
+        "OP_M" => row.OP_M,
+        "OP_N" => row.OP_N,
+        "OP_K" => row.OP_K,
+        "kernel" => row.kernel_str == "pipelined" ? Kernel.matmul_pipelined : Kernel.matmul_singlestage,
+        "is_A_col_major" => has_layout_sweep ? row.is_A_col_major : nothing,
+        "is_B_col_major" => has_layout_sweep ? row.is_B_col_major : nothing,
+        "is_D_col_major" => has_layout_sweep ? row.is_D_col_major : nothing,
+        "PERM_M" => has_layout_sweep ? row.PERM_M : nothing,
+        "PERM_N" => has_layout_sweep ? row.PERM_N : nothing,
+        "PERM_K" => has_layout_sweep ? row.PERM_K : nothing)
+end
 
 function run_tc_gemmkernels(name, extents, optimal_params)
     problem = WMMATensorContraction(; name, extents, data_type, compute_type, accumulate_type, zero_c)
@@ -145,14 +105,12 @@ function main()
     TC = ARGS[1]
     implementation = ARGS[2]
 
-    @assert TC ∈ keys(EXTENTS)
-
     if implementation == "cutensor"
-        run_tc_cutensor(TC, EXTENTS[TC])
+        run_tc_cutensor(TC, get_extents(TC))
     elseif implementation == "gemmkernels-optimal-for-jupiter"
-        run_tc_gemmkernels(TC, EXTENTS[TC], OPTIMAL_PARAMS[(TC, "jupiter")])
+        run_tc_gemmkernels(TC, get_extents(TC), get_optimal_params(TC, "jupiter"))
     elseif implementation == "gemmkernels-optimal-for-ripper"
-        run_tc_gemmkernels(TC, EXTENTS[TC], OPTIMAL_PARAMS[(TC, "ripper")])
+        run_tc_gemmkernels(TC, get_extents(TC), get_optimal_params(TC, "ripper"))
     else
         @assert false
     end
