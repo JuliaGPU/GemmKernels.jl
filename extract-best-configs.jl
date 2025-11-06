66a1d3bff2ef5c41966990a2f6a7961449fe4c7a
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2025-11-06 14:00:13 +0100

    Add script to extract GK and cuTENSOR times

---
+++ extract-best-configs.jl
@@ -0,0 +1,17 @@
+#!/usr/bin/env julia
+
+using DataFrames
+using Serialization
+
+function main()
+    df = open("tuning/best-configs.bin") do io
+        deserialize(io)
+    end
+
+    println("gemmkernels_times,baseline_times")
+    for (i, row) in enumerate(eachrow(df))
+        println("$(join(row.gemmkernels_times, ";")),$(join(row.baseline_times,";"))")
+    end
+end
+
+isinteractive() || main()
